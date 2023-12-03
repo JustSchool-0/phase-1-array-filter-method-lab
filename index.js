840b7e89f7ccd2a5b337cb2b4a8ba7018d9aeca0
Me <pdprince.lfc+github@gmail.com>
2023-12-03 13:41:47 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(drivers, name) {
+  return drivers.filter(driver => driver.toLocaleLowerCase() === name.toLocaleLowerCase());
+}
+
+function fuzzyMatch(drivers, name) {
+  return drivers.filter(driver => driver.toLocaleLowerCase().startsWith(name.toLocaleLowerCase()))
+}
+
+function matchName(drivers, name) {
+  return drivers.filter(driver => driver.name.toLocaleLowerCase() === name.toLocaleLowerCase());
+}
